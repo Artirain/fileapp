f015f1438d73fa30e4dea5bcc1be60de4705eb10
Astoriy <ansre1313@gmail.com>
2023-11-27 22:03:04 +0600

    static3

--- filecrud/settings.py
+++ filecrud/settings.py
@@ -119,13 +119,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+if DEBUG == True:
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static',
 ]
 
-STATIC_ROOT = BASE_DIR /'staticfiles'
+else:
+    STATIC_ROOT = BASE_DIR /'staticfiles'
+
+
+STATIC_URL = '/static/'
+
+
 
 
 MEDIA_URL = '/media/'
